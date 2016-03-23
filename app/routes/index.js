f3336bf421887053510dbb38dff59572991ee68c
Kai <registrierung@skusku.org>
2016-03-23 22:35:46 +0100

    Timestamp microserver

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,17 +1,26 @@
 'use strict';
-
-var ClickHandler = require(process.cwd() + '/app/controllers/clickHandler.server.js');
+var url = require("url");
+var strftime = require("strftime");
 
 module.exports = function (app, db) {
-   var clickHandler = new ClickHandler(db);
-
-   app.route('/')
-      .get(function (req, res) {
-         res.sendFile(process.cwd() + '/public/index.html');
-      });
-
-   app.route('/api/clicks')
-      .get(clickHandler.getClicks)
-      .post(clickHandler.addClick)
-      .delete(clickHandler.resetClicks);
+   app.get("/:data([\\w,%]+)", function (req, res) {
+    var query = unescape(req.url).split("");
+    query.shift();
+    query = query.join("");
+    if(!query.match(/\D/)) {
+      query *= 1000;
+    }
+    var date = new Date(query);
+    if(date == "Invalid Date"){
+      res.send({
+        unix: "null",
+        natural: "null"
+      })
+    } else {
+      res.send({
+        unix: date.getTime()/1000,
+        natural: strftime("%B %d, %Y", date)
+      })
+    }
+  });
 };
